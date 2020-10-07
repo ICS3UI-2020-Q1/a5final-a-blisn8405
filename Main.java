f8735ebe21d339d073052f07b41f99285d2cb3e8
blisn8405 <blisn8405@wrdsb.ca>
2020-10-07 15:31:01 +0000

    final

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner; 
 /**
  *
  * @author 
@@ -9,7 +10,19 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+      Scanner input = new Scanner(System.in);
+    //get users number
+System.out.println("Please enter the length of the line");
+   int number = input.nextInt(); 
+   for(int i = number; i >=1; i--){
+   if(i %2 ==0){
+       System.out.print("white,");
+     } else {
+       if(i %2 !=0){
+       System.out.print("black,");
+       }
+    }
   }
+    }
 }
+  
